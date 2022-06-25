f5fdcf3a86fb58cd995db4dbeef7653ab6a6aca4
TatTo <81563835+Tato1999@users.noreply.github.com>
2022-06-25 16:00:35 +0400

    Add files via upload

---
+++ geBay/index.js
@@ -0,0 +1,11 @@
+/*window.addEventListener("load", TEst);
+function TEst(){
+    let t = document.getElementById('ebayText');
+    if(document.getElementById('seven').onmouseover) {
+        document.getElementById('seven').onmouseover = function(){
+        t.style.visibility = 'visible';
+        console.log('text');
+        }
+    }
+    
+}*/
